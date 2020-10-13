26c3f4a41b166308f8420e6ac6fd94a42faceabd
Sean Morgan <insideseanshead@gmail.com>
2020-10-12 20:22:36 -0700

    employee db and index.js

---
+++ employeeSeeds.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO department(name)
+VALUE ('Boyz');
+
+INSERT INTO role(title, salary)
+VALUE ('Choppa', 123.22);
+
+INSERT INTO employee(first_name,last_name,role_id)
+VALUE ('Vinnie', 'Jonez',1);
+
